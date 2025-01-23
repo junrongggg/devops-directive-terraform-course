9bdd70d7547956fac78beaa192c954c00c8b661b
junrongggg <junrongt4n@gmail.com>
2025-01-23 17:28:27 +0800

    create ec2 instance with main.tf

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -1,17 +1,23 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
+      source = "hashicorp/aws"
+      version = "5.84.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  # Configuration options
+  region = "ap-southeast-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
-  instance_type = "t2.micro"
+resource "aws_instance" "Terraform_ec2" {
+    ami = "ami-0c2e5288624699cd8"
+    instance_type = "t4g.large"
+    subnet_id = "subnet-013f2f15031c839c3"
+
+    tags = {
+    Name = "Terraform_learning"
+  }
 }
